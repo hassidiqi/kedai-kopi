b5e4631303f32b8f38e731d86fa501d1f73d8cac
hasbi <hassidiqi1206@gmail.com>
2023-03-22 06:53:24 +0700

    menyelesaikan navbar

--- js/script.js
+++ js/script.js
@@ -4,3 +4,12 @@ const navbarnav = document.querySelector('.navbar-nav');
 document.querySelector('#hamburger-menu').onclick = () => {
     navbarnav.classList.toggle('active');
 }
+
+// klik di luar sidebar untuk menghilangkan nav
+const hamburger = document.querySelector('#hamburger-menu');
+
+document.addEventListener('click', function(e) {
+    if (!hamburger.contains(e.target) && !navbarnav.contains(e.target)) {
+        navbarnav.classList.remove('active');
+    }
+});
